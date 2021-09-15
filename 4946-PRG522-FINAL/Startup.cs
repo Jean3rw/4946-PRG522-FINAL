3a0b85f5bfb5df815d83377fffb0268ba330ef99
Jean3rw <88708808+Jean3rw@users.noreply.github.com>
2021-09-15 10:38:57 +0200

    Add project files.

---
+++ 4946-PRG522-FINAL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4946_PRG522_FINAL.Startup))]
+namespace _4946_PRG522_FINAL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
